f550902324fff889b4c74c8a2825652ed1bde6e8
AK <akilimar@gmail.com>
2021-03-09 23:10:42 +0100

    Add classess js

---
+++ src/js/classess.js
@@ -0,0 +1,5 @@
+// What is object instantiation in simple words (fisr few comments) https://www.quora.com/What-does-it-mean-to-instantiate-an-object
+
+// Objects - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Working_with_Objects
+
+// Comparing objects models (JavaScript vs Java) -https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Details_of_the_Object_Model
